5d0b8ddffc9ab8bd25d3bffcfe668807558ae988
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2026-09-16 06:34:25 +0200

    Initial Commit

---
+++ ToastsModifier/ToastsModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastsModifierApp.swift
+//  ToastsModifier
+//
+//  Created by Manyuchi, Carrington C on 2026/09/16.
+//
+
+import SwiftUI
+
+@main
+struct ToastsModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
